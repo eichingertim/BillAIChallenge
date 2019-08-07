43f6e499245fa92ad479d15a9b6b2dff550328e8
Tim Eichinger <business.timeichinger@gmail.com>
2019-08-07 17:24:04 +0200

    added firebase firestore

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+var db = firebase.firestore();
+
 const txtEmailRegister = document.getElementById('txt_email_register');
 const txtPasswordRegister = document.getElementById('txt_password_register');
 const btnRegister = document.getElementById('btn_register');
@@ -18,6 +20,10 @@ const btnRegister = document.getElementById('btn_register');
 const txtEmailLogin = document.getElementById('txt_email_login');
 const txtPasswordLogin = document.getElementById('txt_password_login');
 const btnLogin = document.getElementById('btn_login');
+
+const btnSignOut = document.getElementById('btn_sign_out');
+const btnOpenLogin = document.getElementById('btn_open_login');
+const btnOpenRegister = document.getElementById('btn_open_register');
 
 
 //LoginEvent
@@ -47,20 +53,61 @@ btnRegister.addEventListener('click', e => {
     //register
     const promise = auth.createUserWithEmailAndPassword(email, password);
     promise.then(e => {
-        document.getElementById('id02').style.display='none';
+        var user = firebase.auth().currentUser;
+        db.collection("users").doc(user.uid).set({
+            email: email
+        })
+        .then(function() {
+            console.log("Document successfully written!");
+            document.getElementById('id02').style.display='none';
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
     }).catch(e => console.log(e.message));
 
-
 });
+
+btnSignOut.addEventListener('click', e => {
+    firebase.auth().signOut().catch(function(error) {
+        console.log(error.message);
+      });
+});
+
+var ul = document.getElementById('list_items');
 
 //RealTimeAuthentication Listener
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
+        btnOpenLogin.style.display = 'none';
+        btnOpenRegister.style.display = 'none';
+        btnSignOut.style.display = 'block';
+
+        getListUpdates(user);
+
         console.log(user);
       } else {
+        btnOpenLogin.style.display = 'block';
+        btnOpenRegister.style.display = 'block';
+        btnSignOut.style.display = 'none';
         console.log('not logged in');
       }
 });
+
+function getListUpdates(user) {
+    db.collection("users").doc(user.uid).collection("bons").onSnapshot(function(querySnapshot) {
+        ul.innerHTML = "";
+        querySnapshot.forEach(function(doc) {
+            renderList(doc);
+        });
+    });
+}
+
+function renderList(doc) {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(doc.data().title));
+    ul.appendChild(li);
+}
 
 //Add a new entry
 function addEntry() {
@@ -69,10 +116,24 @@ function addEntry() {
     if (entryString == "") {
         alert('Du musst etwas eingeben um es hinzufügen zu können');
     } else {
-        var newElement = document.createElement("LI");
-        var text = document.createTextNode(entryString);
-        newElement.append(text);
-        document.getElementById("list").appendChild(newElement);
+        var user = firebase.auth().currentUser;
+        if (user != null) {
+            db.collection("users").doc(user.uid).collection("bons").add({
+                title: entryString
+            })
+            .then(function(docRef) {
+                document.getElementById("entry_input").value = '';
+                console.log("Document written with ID: ", docRef.id);
+            })
+            .catch(function(error) {
+                console.error("Error adding document: ", error);
+            });
+        } else {
+            alert("Du musst dich einloggen um deiner Liste etwas hinzufügen zu können!");
+        }
+
+        
+
     }
 
 }
